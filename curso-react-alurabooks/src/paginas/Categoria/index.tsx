f2800c7ca06f1c650225a3bc69ec1b2fe8523846
erick <erickbarretodasilva@gmail.com>
2026-03-18 20:29:26 -0300

    definindo menu

--- curso-react-alurabooks/src/paginas/Categoria/index.tsx
+++ curso-react-alurabooks/src/paginas/Categoria/index.tsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
 import TituloPrincipal from "../../componentes/TituloPrincipal";
+import { ICategoria } from "../../interfaces/ICategoria";
+import { useParams } from "react-router-dom";
+import http from "../../http";
 
 export default function Categoria() {
+
+const [categoria, setCategoria] = useState<ICategoria>()
+
+const params = useParams()
+
+useEffect(() => {
+    http.get<ICategoria[]>('categorias', {
+        params: {
+            slug: params.slug
+        }
+    })
+        .then(resposta => {
+            setCategoria(resposta.data[0])
+        })
+}, [params.slug])
+
     return (
         <div>
-            <TituloPrincipal texto="Categoria" />
+            <TituloPrincipal texto={categoria?.nome ?? 'Carregando...'} />
         </div>
     )
 }
