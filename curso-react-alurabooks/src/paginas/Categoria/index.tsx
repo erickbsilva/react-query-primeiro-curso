ba127a8cd5948f40ce81b886ade5ca6eef2f7cc3
erick <erickbarretodasilva@gmail.com>
2026-03-23 18:56:54 -0300

    adição de um loader

--- curso-react-alurabooks/src/paginas/Categoria/index.tsx
+++ curso-react-alurabooks/src/paginas/Categoria/index.tsx
@@ -3,14 +3,18 @@ import TituloPrincipal from "../../componentes/TituloPrincipal";
 import { ICategoria } from "../../interfaces/ICategoria";
 import { useParams } from "react-router-dom";
 import http from "../../http";
+import Loader from "../../componentes/Loader";
 
 export default function Categoria() {
 
 const [categoria, setCategoria] = useState<ICategoria>()
 
+const [carregando, setCarregando] = useState(true)
+
 const params = useParams()
 
 useEffect(() => {
+    setCarregando(true)
     http.get<ICategoria[]>('categorias', {
         params: {
             slug: params.slug
@@ -18,8 +22,18 @@ useEffect(() => {
     })
         .then(resposta => {
             setCategoria(resposta.data[0])
+            setCarregando(false)
+        })
+        .catch(erro => {
+            console.log(erro)
         })
 }, [params.slug])
+
+if (carregando) {
+    return (<div>
+        <Loader />
+    </div>)
+}
 
     return (
         <div>
