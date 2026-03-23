7e1bec6a12d3f7ebe957c9c4d304b93a83db4682
erick <erickbarretodasilva@gmail.com>
2026-03-23 20:07:32 -0300

    adicionando o tan stack

--- curso-react-alurabooks/src/paginas/Categoria/index.tsx
+++ curso-react-alurabooks/src/paginas/Categoria/index.tsx
@@ -2,38 +2,43 @@ import { useEffect, useState } from "react";
 import TituloPrincipal from "../../componentes/TituloPrincipal";
 import { ICategoria } from "../../interfaces/ICategoria";
 import { useParams } from "react-router-dom";
-import http from "../../http";
+import http, { obterCategoriaPorSlug } from "../../http";
 import Loader from "../../componentes/Loader";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Categoria() {
 
-const [categoria, setCategoria] = useState<ICategoria>()
+    // const [categoria, setCategoria] = useState<ICategoria>()
 
-const [carregando, setCarregando] = useState(true)
+    const [carregando, setCarregando] = useState(true)
 
-const params = useParams()
+    const params = useParams()
 
-useEffect(() => {
-    setCarregando(true)
-    http.get<ICategoria[]>('categorias', {
-        params: {
-            slug: params.slug
-        }
-    })
-        .then(resposta => {
-            setCategoria(resposta.data[0])
-            setCarregando(false)
-        })
-        .catch(erro => {
-            console.log(erro)
-        })
-}, [params.slug])
+    // fazendo o destruct, do useQuery, pode já renomear o data para categoria
+    const { data: categoria, isLoading } = useQuery({ queryKey: ['categoriaPorSlug', params.slug], queryFn: () => obterCategoriaPorSlug(params.slug || '') })
 
-if (carregando) {
-    return (<div>
-        <Loader />
-    </div>)
-}
+    // useEffect(() => {
+    //     setCarregando(true)
+    //     http.get<ICategoria[]>('categorias', {
+    //         params: {
+    //             slug: params.slug
+    //         }
+    //     })
+    //         .then(resposta => {
+    //             setCategoria(resposta.data[0])
+    //             setCarregando(false)
+    //         })
+    //         .catch(erro => {
+    //             console.log(erro)
+    //             setCarregando(false)
+    //         })
+    // }, [params.slug])
+
+    if (isLoading) {
+        return (<div>
+            <Loader />
+        </div>)
+    }
 
     return (
         <div>
